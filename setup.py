c3d94bccba3c04fe644d300c74f1db455528d87d
unknown <nik.vaibhav18@gmail.com>
2023-10-25 20:25:07 +0530

    structure setup added

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages,setup
 
+
+
+
 setup(
     name="sensor",
     version="0.0.1",
     author="nik-vaibhav18",
     author_email="nik.vaibhav18@gmail.com",
     packages = find_packages(),
-    install_requires=get_requirements(),#["pymongo==4.2.0"],
+    #install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
