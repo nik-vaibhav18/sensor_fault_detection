7bb9f21c3fc4cefd4759a7d49ac773a3fd875b7c
unknown <nik.vaibhav18@gmail.com>
2023-10-31 18:36:20 +0530

    exception code  added

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.pipeline.exception import SensorException
+import os,sys
+
+def test_Exception():
+    try:
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
 
 
 if __name__ == '__main__':
+
+    try:
+        test_Exception()
+    except Exception as e:
+        print(e)
+
     mongodb_client = MongoDBClient()
     print("collection name:",mongodb_client.database.list_collection_names())
